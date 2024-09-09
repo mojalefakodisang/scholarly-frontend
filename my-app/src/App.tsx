3f704ac60f55393d911fbb2e13338c36e09ca730
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-09-09 12:02:43 +0200

    [Update] - Edited the main page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './favicon.png';
 import './App.css';
 
 function App() {
+  const link: string = "https://github.com/scholarlySA/scholarly-blog";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+            Scholarly South Africa website coming soon.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <small>For more information, visit our<a href={link}> GitHub repository</a>
+        </small>
+       </header>
     </div>
   );
 }
